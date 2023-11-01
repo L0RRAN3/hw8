1b2cc0086116c070459e762bbca771e6638d40e2
L0RRAN3 <lorrane_hvh@mail.ru>
2023-11-01 22:04:18 +0500

    дз точно готово!

--- src/Main.java
+++ src/Main.java
@@ -15,31 +15,45 @@ public class Main {
         float[] second = {1.57f, 7.654f, 9.986f};
 
         System.out.println("");
-        int[] third = new int[10];
+        int[] third = new int[9];
 
         System.out.println("Задача 2");
         System.out.println(Arrays.toString(first));
         System.out.println(Arrays.toString(second));
-        for (int i = 0; i < third.length - 1; i++) {
+        for (int i = 0; i <= third.length - 1; i++) {
             third[i] = i + 1;
             System.out.print(third[i] + ", ");
             third[third.length - 1] = i + 2;
+            if (i == third.length - 1) {
+                System.out.println(third[third.length - 1]);
+            }
         }
-        System.out.println(third[third.length - 1]);
 
         System.out.println("Задача 3");
-        for (int arr = first.length - 1; arr > 0; arr = arr - 1) {
-            System.out.print(first[arr] + ", ");
+        for (int arr = first.length - 1; arr >= 0; arr--) {
+            if (arr > 0) {
+                System.out.print(first[arr] + ", ");
+            }
+            if (arr == 0) {
+                System.out.println(first[0]);
+            }
         }
-        System.out.println(first[0]);
-        for (int sec = second.length - 1; sec > 0; sec = sec - 1) {
-            System.out.print(second[sec] + ", ");
+        for (int sec = second.length - 1; sec >= 0; sec--) {
+            if (sec > 0) {
+                System.out.print(second[sec] + ", ");
+            }
+            if (sec == 0) {
+                System.out.println(second[0]);
+            }
         }
-        System.out.println(second[0]);
-        for (int th = third.length - 1; th > 0; th = th - 1) {
-            System.out.print(third[th] + ", ");
+        for (int th = third.length - 1; th >= 0; th--) {
+            if (th > 0) {
+                System.out.print(third[th] + ", ");
+            }
+            if (th == 0) {
+                System.out.println(third[0]);
+            }
         }
-        System.out.println(third[0]);
         System.out.println("Задача 4");
         for (int ch = 0; ch <= first.length - 1; ch++) {
             if ((first[ch] % 2) != 0) {
